4735912ccf5233e377bb84e53c4aa63edbf2c093
Piotr <piotrgoralewicz25@gmail.com>
2018-12-10 22:06:04 +0100

    reading database structure

--- src/main/java/pl/PiotrG25/sql/SQL.java
+++ src/main/java/pl/PiotrG25/sql/SQL.java
@@ -1,8 +1,12 @@
 package pl.PiotrG25.sql;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQL {
 
@@ -21,9 +25,27 @@ public class SQL {
                 Connection conn = DriverManager.getConnection(connectionString, connectionUser, connectionPassword);
         ){
             System.out.println("Hellow world!");
+
         }catch(SQLException e){
             e.printStackTrace();
         }
 
     }
+
+    /*Reading database structure
+
+        Statement stm = conn.createStatement();
+        ResultSet rs = stm.executeQuery("SELECT * FROM tab1;");
+        ResultSetMetaData rsmd = rs.getMetaData();
+
+        int cols = rsmd.getColumnCount();
+
+        for(int i = 1; i <= cols; i++){
+            System.out.println("Class: " + rsmd.getColumnClassName(i));
+            System.out.println("Name: " + rsmd.getColumnName(i));
+            System.out.println("Type: " + rsmd.getColumnTypeName(i));
+            System.out.println("Precision: " + rsmd.getPrecision(i));
+            System.out.println("Scale: " + rsmd.getScale(i));
+        }
+    */
 }
